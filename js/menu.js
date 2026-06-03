7a8de6e4fda6f83a5577373ab730d9c3ddaca27a
Win654321 <qguy202606@gmail.com>
2026-06-03 23:15:10 +0000

    fix: make menu.js reapply active highlight after footer is mounted

--- js/menu.js
+++ js/menu.js
@@ -1,8 +1,17 @@
 (function() {
   var page = location.pathname.split('/').pop() || 'index.html';
-  var links = document.querySelectorAll('.menu-link');
-  links.forEach(function(a) {
-    var href = a.getAttribute('href') || '';
-    a.classList.toggle('active', href && href.indexOf(page) > -1);
-  });
+
+  function highlight() {
+    var links = document.querySelectorAll('.menu-link');
+    links.forEach(function(a) {
+      var href = a.getAttribute('href') || '';
+      a.classList.toggle('active', href && href.indexOf(page) > -1);
+    });
+  }
+
+  highlight();
+
+  if (window.MutationObserver) {
+    new MutationObserver(highlight).observe(document.body, { childList: true, subtree: true });
+  }
 })();
